5cfff7b1873468006415bb79b1939c28bb0db79e
igorbelousov <belousov.ig@gmail.com>
2021-02-10 12:52:47 +0200

    update

--- internal/tests/tests.go
+++ internal/tests/tests.go
@@ -1,13 +1,19 @@
 package tests
 
 import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"fmt"
 	"log"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/igorbelousov/shop-backend/foundation/database"
+	"github.com/igorbelousov/shop-backend/internal/auth"
 	"github.com/igorbelousov/shop-backend/internal/data/schema"
+	"github.com/igorbelousov/shop-backend/internal/data/user"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -25,6 +31,17 @@ var (
 	AdminID = "5cf37266-3473-4006-984f-9325122678b7"
 	UserID  = "45b5fbd3-755f-4379-8f07-a58d4a30fa2f"
 )
+
+type Test struct {
+	TraceID  string
+	DB       *sqlx.DB
+	Log      *log.Logger
+	Auth     *auth.Auth
+	KID      string
+	Teardown func()
+
+	t *testing.T
+}
 
 func NewUnit(t *testing.T) (*log.Logger, *sqlx.DB, func()) {
 	c := startContainer(t, dbImage, dbPort, dbArgs...)
@@ -84,4 +101,65 @@ func NewUnit(t *testing.T) (*log.Logger, *sqlx.DB, func()) {
 
 func StringPointer(s string) *string {
 	return &s
+}
+
+// NewIntegration creates a database, seeds it, constructs an authenticator.
+func NewIntegration(t *testing.T) *Test {
+	log, db, teardown := NewUnit(t)
+
+	if err := schema.Seed(db); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create RSA keys to enable authentication in our service.
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Build an authenticator using this key lookup function to retrieve
+	// the corresponding public key.
+	kidID := "4754d86b-7a6d-4df5-9c65-224741361492"
+	lookup := func(kid string) (*rsa.PublicKey, error) {
+		switch kid {
+		case kidID:
+			return &privateKey.PublicKey, nil
+		}
+		return nil, fmt.Errorf("no public key found for the specified kid: %s", kid)
+	}
+
+	auth, err := auth.New("RS256", lookup, auth.Keys{kidID: privateKey})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	test := Test{
+		TraceID:  "00000000-0000-0000-0000-000000000000",
+		DB:       db,
+		Log:      log,
+		Auth:     auth,
+		KID:      kidID,
+		t:        t,
+		Teardown: teardown,
+	}
+
+	return &test
+}
+
+// Token generates an authenticated token for a user.
+func (test *Test) Token(email, pass string) string {
+	test.t.Log("Generating token for test ...")
+
+	u := user.New(test.Log, test.DB)
+	claims, err := u.Authenticate(context.Background(), test.TraceID, time.Now(), email, pass)
+	if err != nil {
+		test.t.Fatal(err)
+	}
+
+	token, err := test.Auth.GenerateToken(test.KID, claims)
+	if err != nil {
+		test.t.Fatal(err)
+	}
+
+	return token
 }
