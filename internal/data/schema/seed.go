05536e4920efb37aa9334fa878a0352651c41fab
igorbelousov <belousov.ig@gmail.com>
2021-02-16 12:23:20 +0200

    fix

--- internal/data/schema/seed.go
+++ internal/data/schema/seed.go
@@ -35,12 +35,16 @@ INSERT INTO users (user_id, name, email, roles, password_hash, date_created, dat
 	('45b5fbd3-755f-4379-8f07-a58d4a30fa2f', 'User Gopher', 'user@example.com', '{USER}', '$2a$10$9/XASPKBbJKVfCAZKDH.UuhsuALDr5vVm6VrYA9VFR8rccK86C1hW', '2019-03-24 00:00:00', '2019-03-24 00:00:00')
 	ON CONFLICT DO NOTHING;
 	-- Create category"
-	INSERT INTO categories (category_id, title, slug, parrent_id, description, date_created, date_updated) VALUES
-	('00000000-0000-0000-0000-000000000000', 'First category', 'first-category', '00000000-0000-0000-0000-000000000000', '', '2020-02-04 00:00:00', '2020-02-04 00:00:00')
+	INSERT INTO categories (category_id, title, slug, image, parrent_id, description, meta_description, meta_title, meta_keywords, date_created, date_updated) VALUES
+	('00000000-0000-0000-0000-000000000000', 'First category', 'first-category', 'link-to-image','00000000-0000-0000-0000-000000000000', '', '','','','2020-02-04 00:00:00', '2020-02-04 00:00:00')
+	ON CONFLICT DO NOTHING;
+	INSERT INTO brands
+	(brand_id, title, slug, description, image, meta_description, meta_title, meta_keywords,  date_created, date_updated) VALUES
+	('84fc7ad7-0f6c-4938-9cec-bb8f55953709', 'Brand Title', 'brand-title', 'description text', 'link-to-image', '','','', '2020-02-04 00:00:00', '2020-02-04 00:00:00')
 	ON CONFLICT DO NOTHING;
 	INSERT INTO products
-	(product_id, title, slug, category_id, price,  description, date_created, date_updated) VALUES
-	('9097a8f9-c7c0-4e88-81da-72ec34a1dc79', 'Product Title', 'product-title', '00000000-0000-0000-0000-000000000000', '3535.23', 'description text', '2020-02-04 00:00:00', '2020-02-04 00:00:00')
+	(product_id, title, slug, category_id, brand_id, price,  description, short_description, image, meta_description, meta_title, meta_keywords,  date_created, date_updated) VALUES
+	('9097a8f9-c7c0-4e88-81da-72ec34a1dc79', 'Product Title', 'product-title', '00000000-0000-0000-0000-000000000000', '84fc7ad7-0f6c-4938-9cec-bb8f55953709', '3535.23', 'description text','', 'link-to-image', '','','','2020-02-04 00:00:00', '2020-02-04 00:00:00')
 	ON CONFLICT DO NOTHING;
 `
 
